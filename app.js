8577c604cd6afcdf73b08d308fd6e5a0dbc4e7fc
David Worrell <David.Worrell@asu.edu>
2016-11-03 00:15:03 -0700

    Completed sign in and sign out. Completed Cookies. started post

--- app.js
+++ app.js
@@ -2,34 +2,77 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 var firstName = "Visitor";
-var lastName = '';
+var role = "guest";
 var ROOT_DIR = "html/";
 const Emitter = require('events');
 var emtr = new Emitter();
+var loginLink = '<a href="/login">Click here to log in.</a>';
 
 http.createServer(function (req, res) {
     var urlObj = url.parse(req.url, true, false);
-    console.log(urlObj);
-    var cookie = req.headers["set-cookie"];
-    var html = '{name}';
+    //console.log(urlObj);
 
-    if (cookie) {
-        firstName = cookie.name;
+    var html = '{name}';
+    var qstr = urlObj.query;
+
+    if (req.headers.cookie) {
+        var cookies = getCookie(req);
+        console.log(":::::::" + cookies);
+        console.log(cookies[0] + "--->" + cookies[1]);
+        firstName = cookies[1];
+        role = cookies[3];
+        loginLink = '<a href="/logout">Click here to log out.</a>';
     } else {
-        console.log("no cookie found");
+        loginLink = '<a href="/login">Click here to log in.</a>';
+        console.log("no cookie found.");
     }
 
     if (urlObj.pathname === '/' || urlObj.pathname === '/index.html') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         html = getWebpage('index');
-    } else if (urlObj.pathname === '/login.html') {
+    } else if (urlObj.pathname === '/login') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         html = getWebpage('login');
+    } else if (urlObj.pathname === '/logout') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'set-cookie': [
+                "name=; expires=Thu, 01 Jan 1970 00:00:00 UTC",
+                "role=; expires=Thu, 01 Jan 1970 00:00:00 UTC"
+            ]
+
+        });
+        firstName = 'Visitor';
+        html = getWebpage('logout');
+    } else if (urlObj.pathname === '/post') {
+
+        if (cookies[3] == 'writer') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            html = getWebpage('post');
+        } else {
+            res.writeHead(404);
+        }
+    } else if (urlObj.pathname === '/action_page.html') {
+        console.log("user: " + qstr.user);
+        console.log("password: " + qstr.password);
+        if (qstr.user === qstr.password) {
+            html = getWebpage('index');
+            res.writeHead(200, {
+                'set-cookie': ['name=' + qstr.user,
+                'role=' + qstr.role]
+            });
+            loginLink = '<p>Thank you for loggin in ' + cookies[1] + '</p>';
+            firstName = qstr.user;
+        } else {
+            html = '<p>User name and password do not match.</p>';
+            html += getWebpage('login');
+        }
     }
 
+    html = html.replace('{logged}', loginLink);
     html = html.replace('{name}', firstName);
-    res.end(html);
 
+    res.end(html);
 }).listen(3000, '127.0.0.1');
 
 function getWebpage(siteName) {
@@ -38,11 +81,24 @@ function getWebpage(siteName) {
     webpage += fs.readFileSync(ROOT_DIR + 'footer.html', 'utf8');
     return webpage;
 }
-/*
-var headers = {
-    'Host': 'www.example.com',
-    'Cookie': cookie,
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(data, 'utf8')
-};
-*/
+
+function getCookie(request) {
+    var cookies = [];
+    var rc = request.headers.cookie;
+    var temp = '';
+
+    for (var i = 0; i < rc.length; i++) {
+        if (rc[i] == '=' || rc[i] == ';') {
+            cookies.push(temp);
+            temp = '';
+        } else if (rc[i] == ' ') {
+        } else {
+            temp += rc[i];
+        }
+    }
+    if (temp != '') {
+        cookies.push(temp);
+    }
+    return cookies;
+}
+
